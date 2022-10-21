d322156b296094bfeaa00a71fbce27629c5b7dec
Morranto <965662709@qq.com>
2022-10-21 22:17:12 +0800

    feat:add impl of dir default cloner

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use std::fs::{self, copy, rename, DirEntry, create_dir};
+use std::io;
+use std::path::Path;
+use std::process::{Command, Stdio};
+extern crate fs_extra;
+use fs_extra::copy_items;
+use fs_extra::dir::{self, remove};
+
 pub trait Cloner {
-    fn clone_dir();
+    fn clone_dir(from: &str, to: &str) -> io::Result<()>;
 }
 pub trait FileMapper {
     fn from_json(path: &str) -> Self;
@@ -17,10 +25,22 @@ impl FileMapper for DefaultMapper {
     }
 }
 
-struct DefaultCloner{}
+struct DefaultCloner {}
 impl Cloner for DefaultCloner {
-    fn clone_dir() {
-        todo!()
+    fn clone_dir(from: &str, to: &str) -> io::Result<()> {
+        if !Path::new(to).exists() {
+            create_dir(to)?;
+        }
+        let path = Path::new(from);
+        for entry in fs::read_dir(path)? {
+            let path = entry?.path();
+            let options = dir::CopyOptions::new();
+            let mut from_paths = Vec::new();
+            let path_str = path.to_str().unwrap();
+            from_paths.push(path_str);
+            copy_items(&from_paths, to, &options).unwrap();
+        }
+        Ok(())
     }
 }
 
@@ -30,4 +50,14 @@ fn main() {
     //read rules of filemapper
 
     //traverse the dir and do map-job
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{DefaultCloner, Cloner};
+
+    #[test]
+    fn dir_clone() {
+        DefaultCloner::clone_dir("src", "doc").unwrap();
+    }
 }
